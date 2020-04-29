5427dac7749ad8a6d71eb2c7255e12d9d6446196
Chandu J S <app.coder@outlook.in>
2020-04-29 11:58:52 +0530

    form id set added

--- api/send.js
+++ api/send.js
@@ -9,7 +9,6 @@ module.exports = async (request, response) => {
       'SMTP_USERNAME',
       'SMTP_PASSWORD',
       'ALLOWED_ORIGINS',
-      'FORM_SET',
     ]
     for (const param of envParams) {
       if (!process.env[param]) {
@@ -22,41 +21,41 @@ module.exports = async (request, response) => {
       throw Error('Permission denied! Origin not verified.')
     }
 
-    const formSets = process.env.FORM_SET.split(',').map((item) => {
-      const dataSet = item.split(':')
-      return {
-        formId: dataSet[0],
-        toEmail: dataSet[1],
-        fromString: dataSet[2],
-        subject: dataSet[3],
-      }
-    })
-
     const body = JSON.parse(request.body)
-    const formSet = formSets.find((item) => item.formId === body.formId)
-    if (!formSet) {
+    const formSet = {
+      fromEmail: process.env[`FORM_${body.formId}_FROM_EMAIL`],
+      fromName: process.env[`FORM_${body.formId}_FROM_NAME`],
+      toEmail: process.env[`FORM_${body.formId}_TO_EMAIL`],
+      subject: process.env[`FORM_${body.formId}_SUBJECT`],
+    }
+    if (
+      formSet.fromEmail &&
+      formSet.fromName &&
+      formSet.toEmail &&
+      formSet.subject
+    ) {
+      const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: parseInt(process.env.SMTP_PORT),
+        secure: process.env.SMTP_SECURE === 'true',
+        auth: {
+          user: process.env.SMTP_USERNAME,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      })
+
+      await transporter.sendMail({
+        from: `"${formSet.fromName}" <${formSet.fromEmail}>`,
+        to: formSet.toEmail,
+        subject: formSet.subject,
+        text: JSON.stringify(body.data),
+        html: JSON.stringify(body.data),
+      })
+
+      response.json({ success: true })
+    } else {
       throw Error('Form not found!')
     }
-
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: parseInt(process.env.SMTP_PORT),
-      secure: process.env.SMTP_SECURE === 'true',
-      auth: {
-        user: process.env.SMTP_USERNAME,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    })
-
-    await transporter.sendMail({
-      from: formSet.fromString,
-      to: formSet.toEmail,
-      subject: formSet.subject,
-      text: JSON.stringify(body.data),
-      html: JSON.stringify(body.data),
-    })
-
-    response.json({ success: true })
   } catch (error) {
     response.json({
       success: false,
